9852b9622bf1096267798e4ecc70402f04e792f6
Lukas Streckeisen <lukas@streckeisen.dev>
2025-03-05 10:55:30 +0100

    add placeholder test

---
+++ src/test/kotlin/org/contextmapper/dsl/intellij/PlaceholderTest.kt
@@ -0,0 +1,11 @@
+package org.contextmapper.dsl.intellij
+
+import org.junit.Test
+
+class PlaceholderTest {
+    @Test
+    fun test() {
+        println("I'm a placeholder! Remove me!")
+        assert(true)
+    }
+}
