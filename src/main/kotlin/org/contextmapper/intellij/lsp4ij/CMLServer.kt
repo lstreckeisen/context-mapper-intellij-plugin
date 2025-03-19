ceb3e24a515ba4577da77c59c6bc02374986be23
Lukas Streckeisen <lukas@streckeisen.dev>
2025-03-19 15:39:13 +0100

    add constructor args to cml server for better testability, add CMLServer test for windows/linux executable test

--- src/main/kotlin/org/contextmapper/intellij/lsp4ij/CMLServer.kt
+++ src/main/kotlin/org/contextmapper/intellij/lsp4ij/CMLServer.kt
@@ -1,10 +1,8 @@
 package org.contextmapper.intellij.lsp4ij
 
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.ide.plugins.PluginManagerCore
-import com.intellij.openapi.extensions.PluginId
+import com.intellij.openapi.extensions.PluginDescriptor
 import com.redhat.devtools.lsp4ij.server.OSProcessStreamConnectionProvider
-import org.apache.commons.lang3.SystemUtils
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger(CMLServer::class.java)
@@ -13,19 +11,17 @@ private const val WINDOWS_LSP_EXECUTABLE = "context-mapper-lsp.bat"
 private const val LINUX_LSP_EXECUTABLE = "context-mapper-lsp"
 private const val LSP_EXECUTABLE_PATH = "lib/lsp/bin"
 
-private const val PLUGIN_ID = "org.contextmapper.intellij"
-
-class CMLServer : OSProcessStreamConnectionProvider() {
+class CMLServer(
+    pluginDescriptor: PluginDescriptor,
+    isOsWindows: Boolean
+) : OSProcessStreamConnectionProvider() {
     init {
         val startScript =
-            if (SystemUtils.IS_OS_WINDOWS) {
+            if (isOsWindows) {
                 WINDOWS_LSP_EXECUTABLE
             } else {
                 LINUX_LSP_EXECUTABLE
             }
-
-        val pluginDescriptor = PluginManagerCore.getPlugin(PluginId.getId(PLUGIN_ID))
-        requireNotNull(pluginDescriptor) { "Could not find Context Mapper plugin descriptor!" }
 
         val lspResourcePath =
             pluginDescriptor.pluginPath
--- src/main/kotlin/org/contextmapper/intellij/lsp4ij/CMLServerFactory.kt
+++ src/main/kotlin/org/contextmapper/intellij/lsp4ij/CMLServerFactory.kt
@@ -1,16 +1,22 @@
 package org.contextmapper.intellij.lsp4ij
 
+import com.intellij.ide.plugins.PluginManagerCore
+import com.intellij.openapi.extensions.PluginId
 import com.intellij.openapi.project.Project
 import com.redhat.devtools.lsp4ij.LanguageServerFactory
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider
+import org.apache.commons.lang3.SystemUtils
+
+private const val PLUGIN_ID = "org.contextmapper.intellij"
 
 class CMLServerFactory : LanguageServerFactory {
     override fun createConnectionProvider(p0: Project): StreamConnectionProvider {
-        return CMLServer()
+        val pluginDescriptor = PluginManagerCore.getPlugin(PluginId.getId(PLUGIN_ID))
+        requireNotNull(pluginDescriptor) { "Could not find Context Mapper plugin descriptor!" }
+
+        return CMLServer(pluginDescriptor, SystemUtils.IS_OS_WINDOWS)
     }
 
-    override fun createLanguageClient(project: Project): LanguageClientImpl {
-        return CMLClient(project)
-    }
+    override fun createLanguageClient(project: Project): LanguageClientImpl = CMLClient(project)
 }
