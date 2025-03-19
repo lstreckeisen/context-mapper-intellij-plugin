6d7154980b88dfafdeec5e0eb5c3d5de58c4446c
Lukas Streckeisen <lukas@streckeisen.dev>
2025-03-19 13:45:52 +0100

    use batch script to start language server on windows

--- src/main/kotlin/org/contextmapper/intellij/lsp4ij/CMLServer.kt
+++ src/main/kotlin/org/contextmapper/intellij/lsp4ij/CMLServer.kt
@@ -4,17 +4,25 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.extensions.PluginId
 import com.redhat.devtools.lsp4ij.server.OSProcessStreamConnectionProvider
+import org.apache.commons.lang3.SystemUtils
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger(CMLServer::class.java)
 
 class CMLServer : OSProcessStreamConnectionProvider() {
     init {
+        val startScript =
+            if (SystemUtils.IS_OS_WINDOWS) {
+                "context-mapper-lsp.bat"
+            } else {
+                "context-mapper-lsp"
+            }
+
         val pluginDescriptor = PluginManagerCore.getPlugin(PluginId.getId("org.contextmapper.intellij"))
         requireNotNull(pluginDescriptor) { "Could not find Context Mapper plugin descriptor!" }
 
         val lspResourcePath =
-            pluginDescriptor.pluginPath.resolve("lib/lsp/bin/context-mapper-lsp")
+            pluginDescriptor.pluginPath.resolve("lib/lsp/bin/$startScript")
                 .toAbsolutePath()
                 .toString()
 
