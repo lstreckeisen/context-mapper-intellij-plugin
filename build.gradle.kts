adf39de2234c217ab63fbef8c25f5c7332584735
Lukas Streckeisen <lukas@streckeisen.dev>
2025-05-08 16:27:21 +0200

    set version to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ intellijPlatform {
     pluginConfiguration {
         id = "org.contextmapper.intellij-plugin"
         name = "ContextMapper"
-        version = "0.1.0"
+        version = "0.2.0"
 
         productDescriptor {
         }
